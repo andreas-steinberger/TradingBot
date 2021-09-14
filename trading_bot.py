b8c2fdcce37f8f909bc6f025dbffa9270630423e
Stoans <86928774+Stoans@users.noreply.github.com>
2021-09-14 12:47:30 +0200

    Add files via upload

--- trading_bot.py
+++ trading_bot.py
@@ -1,5 +1,5 @@
 import time
-import csv
+import sqlite3
 from binance import Client
 from datetime import datetime
 from sys import platform  # get platform, windows or ubuntu
@@ -14,6 +14,7 @@ def get_api():
             api.append(line[-1])
     return api
 
+
 '''API Key and API Secret'''
 API_KEY, API_SECRET = get_api()
 
@@ -23,35 +24,61 @@ TO_TRADE = CRYPTO[0][0]
 QUANTITY = CRYPTO[0][1]
 
 '''Settings'''
-RANGE = 60  # how much time to look back
+RANGE = 10  # 30  # how much time to look back
 TOP = 1.0002  # good case
 BOT = 0.995  # bad case, sell
 
 '''Logfile path'''
-NAME = TO_TRADE + str(int(time.time())) + '.csv'  # what to trade and start time
-UBUNTU_PATH = "/home/ubuntu/TradingBot/"  # path on the aws server
+_NAME = "Logfile.db"  # what to trade and start time
+_UBUNTU_PATH = "/home/ubuntu/TradingBot/"  # path on the aws server
 
-LOGFILE_NAME = NAME if platform == "win32" else UBUNTU_PATH + NAME  # actual logfile path
+LOGFILE_NAME = _NAME if platform == "win32" else _UBUNTU_PATH + _NAME  # actual logfile path
+
+
+class SQL:
+    '''SQL database for logfiles, after creating only 'insert_data()' is needed'''
+    def __init__(self):
+        self.conn = sqlite3.connect(LOGFILE_NAME)
+        self.table_name = TO_TRADE + str(int(time.time()))
+        self.create_table()
+
+    def create_table(self):
+        content = "Timestamp text, Symbol text, Quantity real, Price real"
+        mode = f"CREATE TABLE {self.table_name}"
+        self.insert_data(content, mode)
+
+    def insert_data(self, content, mode=f"INSERT INTO <table_name> VALUES"):
+        c = self.conn.cursor()
+
+        string = str(content)
+        mode = mode.replace("<table_name>", self.table_name)
+        c.execute(f"{mode}({string})")  # string = "abc", "def", 100, 10.5
+
+        self.conn.commit()
 
 
 class HelpFunctions:
     def __init__(self, client):
         self.client = client
 
+    '''description'''
+    def place_order(self, mode, t):
+        try:
+            if mode == 'buy':
+                return self.client.order_market_buy(symbol=TO_TRADE, quantity=QUANTITY)
+            elif mode == 'sell':
+                return self.client.order_market_sell(symbol=TO_TRADE, quantity=QUANTITY)
+        except:
+            # self.write_csv([["Bad Connection, place order"]])
+            return False
+
     '''give back the current margin price, float'''
     def get_price(self) -> float:
         try:
             return float(self.client.get_margin_price_index(symbol=TO_TRADE)['price'])
         except:
-            self.write_csv([["Bad Connection"]])
+            # self.write_csv([["Bad Connection, get price"]])
             self.get_price()
-
-    '''write csv logfile and save it to logfile name'''
-    def write_csv(self, to_write, m='a'):
-        with open(LOGFILE_NAME, m) as file:
-            writer = csv.writer(file, delimiter=';')
-            for row in to_write:
-                writer.writerow(row)
 
 
 class TradingBot(HelpFunctions):
@@ -69,42 +96,50 @@ class TradingBot(HelpFunctions):
         # create instance of HelpingFunctions
         super().__init__(self.client)
 
+        # create instance of SQL to write logfiles
+        self.s = SQL()
+
     def start(self):
         is_bought = False  # give back the current state
         self.price_when_started = self.get_price()  # get current price in float
-        delta = 1.0  # time delta how long to wait
 
         '''create logfile'''
-        to_write = [["Started at", datetime.fromtimestamp(time.time())],
-                    ["Price when started: ", str(self.price_when_started).replace('.', ','), "USD"]]
-        self.write_csv(to_write, m='w')
+        # to_write = [["Started at", datetime.fromtimestamp(time.time())],
+        #            ["Price when started: ", str(self.price_when_started).replace('.', ','), "USD"]]
+        # self.write_csv(to_write, m='w')
 
         '''main loop'''
         while True:
+            t = time.time()  # time at the beginning of the loop, float
             curr_price = self.get_price()  # get current price
-            t = time.time()  # get time, float
 
             '''add current price to list and delete the oldest one'''
             self.last_prices.append(curr_price)
             del self.last_prices[0]
 
-            # only temp
-            time.sleep(0.2)
-
             '''first state: nothing is bought'''
             if not is_bought:
-                pass
+                change = self.last_prices[-1] / self.last_prices[0]  # percent, e.g. 0.998, 1.002
+                print(change)
+                if change > 1.0:
+                    order = self.place_order('buy', t)
+                    if order:
+                        is_bought = True
+                        self.bought_at = float(order['fills'][0]['price'])
+
+                    # to_write = [["to write"]]
+                    # self.write_csv(to_write)
 
             '''second state: crypto is bought'''
             if is_bought:
                 pass
 
             '''calculate time to wait, should be 1 second for one loop'''
-            wait = 1.0 - delta
-            time.sleep(wait)
-            t_end = time.time()
-            delta = t_end - t
-            print(delta)
+            t_end = time.time()  # time at the and of the loop, float
+            wait = 0.99 - (t_end - t)
+            if wait > 0.0:
+                time.sleep(wait)
+            # duration = time.time() - t  # complete time with waiting time
 
 
 if __name__ == '__main__':
