a64b5b91e8e629e131d266332adeff3310d32ebf
Stoans <86928774+Stoans@users.noreply.github.com>
2021-09-14 19:37:10 +0200

    Add files via upload

--- sql_analyse.py
+++ sql_analyse.py
@@ -1,1 +1,11 @@
-# sql analyse
+import sqlite3
+
+
+table_name = "BTCUSDT1631632130"
+
+conn = sqlite3.connect("Logfile.db")
+c = conn.cursor()
+
+c.execute(f"SELECT * FROM {table_name}")
+content = c.fetchall()
+print(content)
